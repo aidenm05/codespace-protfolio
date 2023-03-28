f1c24cb5ec6cef4229df4e68281a7e27d35469cd
aiden mcmanus <89177158+aidenm05@users.noreply.github.com>
2023-03-28 18:37:01 +0000

    s

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,23 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a web designer Student, creator, and aspiring computer scientist. I am passionate about solving problems in new creative ways to drive innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web design and Ui/UX",
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "Java",
+  "Electronics Design and Fabrication",
+  "First Robotics",
+  "Git",
+  "GitHub",
+  "Linux",
 ];
 
 /**
@@ -45,7 +49,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I enjoy working with people and learning new things.I enjoy working on projects that are challenging and require me to think outside the box.";
 
 const About = () => {
   return (
